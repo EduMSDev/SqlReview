7ff8e143571f449d165637db7bf2c653c71737f2
Edu <edumartsil95@tutanota.com>
2022-01-10 09:02:43 +0100

    Added new exercices files 15 and 16

---
+++ Exercices video 15 cross-referencing and 16 DDL.sql
@@ -0,0 +1,7 @@
+-- Exercices video 15 cross-referencing
+-- Only in ACCESS Microsoft
+-- Exercices video 16 DDL
+CREATE TABLE PRUEBA (id_alumno int AUTO_INCREMENT, nombre varchar (20), apellido varchar(20), 
+	edad TINYINT, fecha_nacimiento date, carnet bool, PRIMARY KEY (id_alumno));
+
+DROP TABLE prueba;
